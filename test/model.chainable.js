7c87a92e6eb7059dbb9aba452bd9ef88bcfdc5d2
Kersten Burkhardt <kerstenk@gmail.com>
2013-07-09 23:17:31 +0200

    Update model.chainable.js
    Fixed identation.

--- test/model.chainable.js
+++ test/model.chainable.js
@@ -31,16 +31,16 @@ describe('Model', function() {
     posts.delete_options.should.be.a('object');
     posts.templateRoot.should.eql('/idk/where/this/goes');
   });
-    it('should be updateable', function() {
-        var Posts = restful.model('posts');
-        Posts.create({
-            title: "First post"
-        }, function(err, post) {
-            post.title.should.equal('First post');
-            Posts.update({_id: post._id}, { title: "Second post"}, function(err, numUpdated, post) {
-                numUpdated.should.equal(1);
-                post.ok.should.equal(1);
-            });
-        })
-    });
+  it('should be updateable', function() {
+    var Posts = restful.model('posts');
+    Posts.create({
+      title: "First post"
+    }, function(err, post) {
+      post.title.should.equal('First post');
+      Posts.update({_id: post._id}, { title: "Second post"}, function(err, numUpdated, post) {
+        numUpdated.should.equal(1);
+        post.ok.should.equal(1);
+      });
+    })
+  });
 });
